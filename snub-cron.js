7d0e764bc6d379c349b88bb2c9e392fc39f7b48a
Patrik Hartwig <git@pat.wtf>
2020-07-27 16:57:59 +1000

    refact

--- snub-cron.js
+++ snub-cron.js
@@ -6,29 +6,28 @@ module.exports = function (config) {
     driftMs: 1000 * 60,
   }, config || {});
 
-  var tracked = [];
+  var tracked = {};
   return function (snub) {
     snub.cron = function (namespace, cronExpression, cronOptions) {
       cronOptions = Object.assign({}, cronOptions, { scheduled: true });
-      tracked.push({
+
+      tracked[namespace] = {
         namespace,
         cronExpression,
         cron: cron.schedule(cron, _ => {
           snub.redis.sadd('_snub-cron:schedules', namespace);
           setTimeout(async _ => {
-            var nameSpaces = await snub.redis.pipeline([['smembers', '_snub-cron:schedules'], ['del', '_snub-cron:schedules']]);
-            if (nameSpaces[0].length)
-              for (const n of nameSpaces[0])
-                snub.mono('cron:' + n);
+            if (!await snub.redis.srem('_snub-cron:schedules', namespace)) return;
+            snub.mono('cron:' + namespace);
           }, config.driftMs);
         })
-      });
+      };
     };
 
     snub.cronDestroy = function (namespace) {
-      var cronIdx = tracked.findIndex(c => c.namespace === namespace);
-      tracked[cronIdx].cron.destroy();
-      tracked.splice(cronIdx, 1);
+      if (!tracked[namespace]) return;
+      tracked[namespace].cron.destroy();
+      delete tracked[namespace];
     };
   };
 };
